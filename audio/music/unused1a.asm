e87560c20c21a4fe8b7524ee014cb2574e062213
lsmmega <pogmegalsm@outlook.com>
2024-05-31 19:14:08 +0800

    Create unused1a.asm

---
+++ audio/music/unused1a.asm
@@ -0,0 +1,27 @@
+music_unused1a:
+	music_pointers
+	dh music_unused1a_pulse1
+	dl music_unused1a_pulse1
+	dh music_unused1a_pulse2
+	dl music_unused1a_pulse2
+	dh music_unused1a_triangle
+	dl music_unused1a_triangle
+	dh music_unused1a_noise
+	dl music_unused1a_noise
+
+music_unused1a_pulse1:
+	tempo $0022
+	note_sustain_length $00
+	volume $0
+	instrument $00
+	octave 0
+	channel_end
+
+music_unused1a_pulse2:
+	channel_end
+
+music_unused1a_triangle:
+	channel_end
+
+music_unused1a_noise:
+	channel_end
