6a6a52d0d48a2b6badd3fcd16354b9ae4eaaef46
lsmmega <pogmegalsm@outlook.com>
2024-06-08 20:07:24 +0800

    Create shoot.asm

---
+++ audio/sfx/shoot.asm
@@ -0,0 +1,17 @@
+sfx_shoot:
+;1
+	sfx_priority $07
+	sfx_global_flags $2
+	sfx_global_flags_bits = $2
+	sfx_global_note_sustain_length $eb
+	sfx_frames $0a
+	sfx_channel_flags $2
+	sfx_channel_flags_bits = $2
+	sfx_pulse2_command_flags $0f
+	sfx_pulse2_command_flags_bits = $0f
+	sfx_pulse2_instrument $2b
+	sfx_pulse2_duty_cycle 1
+	sfx_pulse2_volume $f
+	sfx_pulse2_pitch_slide $ff
+	sfx_pulse2_note D_4
+	sfx_end
