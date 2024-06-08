cd435f2076ac7e81a450c5c771fe51e0de3b2dd3
lsmmega <pogmegalsm@outlook.com>
2024-06-08 20:07:12 +0800

    Create death.asm

---
+++ audio/sfx/death.asm
@@ -0,0 +1,104 @@
+sfx_death:
+;1
+	sfx_priority $15
+	sfx_global_flags $2
+	sfx_global_flags_bits = $2
+	sfx_global_note_sustain_length $ff
+	sfx_frames $0e
+	sfx_channel_flags $3
+	sfx_channel_flags_bits = $3
+	sfx_pulse1_command_flags $0f
+	sfx_pulse1_command_flags_bits = $0f
+	sfx_pulse1_instrument $2a
+	sfx_pulse1_duty_cycle 2
+	sfx_pulse1_volume $f
+	sfx_pulse1_pitch_slide $7f
+	sfx_pulse1_note C#5
+	sfx_pulse2_command_flags $0f
+	sfx_pulse2_command_flags_bits = $0f
+	sfx_pulse2_instrument $2a
+	sfx_pulse2_duty_cycle 1
+	sfx_pulse2_volume $f
+	sfx_pulse2_pitch_slide $7f
+	sfx_pulse2_note C#5
+
+;2
+	sfx_global_flags $2
+	sfx_global_flags_bits = $2
+	sfx_global_note_sustain_length $ff
+	sfx_frames $0e
+	sfx_channel_flags $3
+	sfx_channel_flags_bits = $3
+	sfx_pulse1_command_flags $04
+	sfx_pulse1_command_flags_bits = $04
+	sfx_pulse1_volume $c
+	sfx_pulse1_note C#5
+	sfx_pulse2_command_flags $04
+	sfx_pulse2_command_flags_bits = $04
+	sfx_pulse2_volume $c
+	sfx_pulse2_note C#5
+
+;3
+	sfx_global_flags $2
+	sfx_global_flags_bits = $2
+	sfx_global_note_sustain_length $ff
+	sfx_frames $0e
+	sfx_channel_flags $3
+	sfx_channel_flags_bits = $3
+	sfx_pulse1_command_flags $04
+	sfx_pulse1_command_flags_bits = $04
+	sfx_pulse1_volume $9
+	sfx_pulse1_note C#5
+	sfx_pulse2_command_flags $04
+	sfx_pulse2_command_flags_bits = $04
+	sfx_pulse2_volume $9
+	sfx_pulse2_note C#5
+
+;4
+	sfx_global_flags $2
+	sfx_global_flags_bits = $2
+	sfx_global_note_sustain_length $ff
+	sfx_frames $0e
+	sfx_channel_flags $3
+	sfx_channel_flags_bits = $3
+	sfx_pulse1_command_flags $04
+	sfx_pulse1_command_flags_bits = $04
+	sfx_pulse1_volume $6
+	sfx_pulse1_note C#5
+	sfx_pulse2_command_flags $04
+	sfx_pulse2_command_flags_bits = $04
+	sfx_pulse2_volume $6
+	sfx_pulse2_note C#5
+
+;5
+	sfx_global_flags $2
+	sfx_global_flags_bits = $2
+	sfx_global_note_sustain_length $ff
+	sfx_frames $0e
+	sfx_channel_flags $3
+	sfx_channel_flags_bits = $3
+	sfx_pulse1_command_flags $04
+	sfx_pulse1_command_flags_bits = $04
+	sfx_pulse1_volume $3
+	sfx_pulse1_note C#5
+	sfx_pulse2_command_flags $04
+	sfx_pulse2_command_flags_bits = $04
+	sfx_pulse2_volume $3
+	sfx_pulse2_note C#5
+
+;6
+	sfx_global_flags $2
+	sfx_global_flags_bits = $2
+	sfx_global_note_sustain_length $ff
+	sfx_frames $0e
+	sfx_channel_flags $3
+	sfx_channel_flags_bits = $3
+	sfx_pulse1_command_flags $04
+	sfx_pulse1_command_flags_bits = $04
+	sfx_pulse1_volume $1
+	sfx_pulse1_note C#5
+	sfx_pulse2_command_flags $04
+	sfx_pulse2_command_flags_bits = $04
+	sfx_pulse2_volume $1
+	sfx_pulse2_note C#5
+	sfx_end
