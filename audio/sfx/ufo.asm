97800fc98aec367f20635e3019b15696dfd1701e
lsmmega <pogmegalsm@outlook.com>
2024-06-08 20:07:27 +0800

    Create ufo.asm

---
+++ audio/sfx/ufo.asm
@@ -0,0 +1,46 @@
+sfx_ufo:
+;1
+	sfx_priority $07
+
+sfx_ufo_main_loop:
+	sfx_global_flags $2
+	sfx_global_flags_bits = $2
+	sfx_global_note_sustain_length $ff
+	sfx_frames $0a
+	sfx_channel_flags $6
+	sfx_channel_flags_bits = $6
+	sfx_pulse2_command_flags $17
+	sfx_pulse2_command_flags_bits = $17
+	sfx_pulse2_instrument $3b
+	sfx_pulse2_duty_cycle 0
+	sfx_pulse2_volume $f
+	sfx_pulse2_pitch_tune $07
+	sfx_pulse2_note D_7_
+	sfx_triangle_command_flags $05
+	sfx_triangle_command_flags_bits = $05
+	sfx_triangle_instrument $3b
+	sfx_triangle_fraction $ff
+	sfx_triangle_note D#6
+
+;2
+	sfx_global_flags $0
+	sfx_global_flags_bits = $0
+	sfx_frames $0a
+	sfx_channel_flags $6
+	sfx_channel_flags_bits = $6
+	sfx_pulse2_command_flags $00
+	sfx_pulse2_command_flags_bits = $00
+	sfx_pulse2_note D_7_
+	sfx_triangle_command_flags $04
+	sfx_triangle_command_flags_bits = $04
+	sfx_triangle_fraction $00
+	sfx_triangle_note D#6
+
+;3
+	sfx_global_flags $1
+	sfx_global_flags_bits = $1
+	sfx_global_loop 0, sfx_ufo_main_loop
+	sfx_frames $00
+	sfx_channel_flags $0
+	sfx_channel_flags_bits = $0
+	sfx_end
