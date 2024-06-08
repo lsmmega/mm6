5f1baf7d8f53b73222e99988dbf1ce31d32b8b53
lsmmega <pogmegalsm@outlook.com>
2024-06-08 20:07:19 +0800

    Create lighting.asm

---
+++ audio/sfx/lighting.asm
@@ -0,0 +1,95 @@
+sfx_lighting:
+;1
+	sfx_priority $07
+	sfx_global_flags $2
+	sfx_global_flags_bits = $2
+	sfx_global_note_sustain_length $90
+	sfx_frames $03
+	sfx_channel_flags $a
+	sfx_channel_flags_bits = $a
+	sfx_pulse2_command_flags $0d
+	sfx_pulse2_command_flags_bits = $0d
+	sfx_pulse2_instrument $36
+	sfx_pulse2_volume $c
+	sfx_pulse2_pitch_slide $ff
+	sfx_pulse2_note A_7_
+	sfx_noise_command_flags $05
+	sfx_noise_command_flags_bits = $05
+	sfx_noise_instrument $28
+	sfx_noise_volume $f
+	sfx_noise_note 11
+
+;2
+	sfx_global_flags $2
+	sfx_global_flags_bits = $2
+	sfx_global_note_sustain_length $90
+	sfx_frames $03
+	sfx_channel_flags $a
+	sfx_channel_flags_bits = $a
+	sfx_pulse2_command_flags $00
+	sfx_pulse2_command_flags_bits = $00
+	sfx_pulse2_note D_7
+	sfx_noise_command_flags $00
+	sfx_noise_command_flags_bits = $00
+	sfx_noise_note 12
+
+;3	
+	sfx_global_flags $2
+	sfx_global_flags_bits = $2
+	sfx_global_note_sustain_length $6C
+	sfx_frames $01
+	sfx_channel_flags $a
+	sfx_channel_flags_bits = $a
+	sfx_pulse2_command_flags $00
+	sfx_pulse2_command_flags_bits = $00
+	sfx_pulse2_note A_7
+	sfx_noise_command_flags $05
+	sfx_noise_command_flags_bits = $05
+	sfx_noise_instrument $28
+	sfx_noise_volume $d
+	sfx_noise_note 11
+
+;4
+	sfx_global_flags $2
+	sfx_global_flags_bits = $2
+	sfx_global_note_sustain_length $90
+	sfx_frames $02
+	sfx_channel_flags $a
+	sfx_channel_flags_bits = $a
+	sfx_pulse2_command_flags $00
+	sfx_pulse2_command_flags_bits = $00
+	sfx_pulse2_note G_7
+	sfx_noise_command_flags $04
+	sfx_noise_command_flags_bits = $04
+	sfx_noise_volume $f
+	sfx_noise_note 8
+
+;5
+	sfx_global_flags $2
+	sfx_global_flags_bits = $2
+	sfx_global_note_sustain_length $80
+	sfx_frames $02
+	sfx_channel_flags $8
+	sfx_channel_flags_bits = $8
+	sfx_noise_command_flags $00
+	sfx_noise_command_flags_bits = $00
+	sfx_noise_note 5
+
+;6
+	sfx_global_flags $2
+	sfx_global_flags_bits = $2
+	sfx_global_note_sustain_length $90
+	sfx_frames $18
+	sfx_channel_flags $a
+	sfx_channel_flags_bits = $a
+	sfx_pulse2_command_flags $0c
+	sfx_pulse2_command_flags_bits = $0c
+	sfx_pulse2_volume $7
+	sfx_pulse2_pitch_slide $7f
+	sfx_pulse2_note D_1
+	sfx_noise_command_flags $09
+	sfx_noise_command_flags_bits = $09
+	sfx_noise_instrument $0c
+	sfx_noise_pitch_slide $4d
+	sfx_noise_note 12
+	sfx_end
