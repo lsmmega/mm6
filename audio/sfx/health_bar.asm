779da916cbbd671568ae5e030b96b94f1fa0ef4f
lsmmega <pogmegalsm@outlook.com>
2024-06-08 20:07:16 +0800

    Create health_bar.asm

---
+++ audio/sfx/health_bar.asm
@@ -0,0 +1,32 @@
+sfx_health_bar:
+;1
+	sfx_priority $14
+
+sfx_health_bar_main_loop:
+	sfx_global_flags $2
+	sfx_global_flags_bits = $2
+	sfx_global_note_sustain_length $ff
+	sfx_frames $04
+	sfx_channel_flags $3
+	sfx_channel_flags_bits = $3
+	sfx_pulse1_command_flags $07
+	sfx_pulse1_command_flags_bits = $07
+	sfx_pulse1_instrument $29
+	sfx_pulse1_duty_cycle 1
+	sfx_pulse1_volume $f
+	sfx_pulse1_note C_5
+	sfx_pulse2_command_flags $07
+	sfx_pulse2_command_flags_bits = $07
+	sfx_pulse2_instrument $29
+	sfx_pulse2_duty_cycle 1
+	sfx_pulse2_volume $f
+	sfx_pulse2_note E_5
+
+;2
+	sfx_global_flags $1
+	sfx_global_flags_bits = $1
+	sfx_global_loop 2, sfx_health_bar_main_loop
+	sfx_frames $00
+	sfx_channel_flags $0
+	sfx_channel_flags_bits = $0
+	sfx_end
