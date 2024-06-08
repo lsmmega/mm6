84536faeba2d423c9eb851fe60b0506809858996
lsmmega <pogmegalsm@outlook.com>
2024-06-08 20:07:15 +0800

    Create fully_charged_shoot.asm

---
+++ audio/sfx/fully_charged_shoot.asm
@@ -0,0 +1,41 @@
+sfx_fully_charged_shoot:
+;1
+	sfx_priority $07
+	sfx_global_flags $2
+	sfx_global_flags_bits = $2
+	sfx_global_note_sustain_length $ff
+	sfx_frames $07
+	sfx_channel_flags $c
+	sfx_channel_flags_bits = $c
+	sfx_triangle_command_flags $05
+	sfx_triangle_command_flags_bits = $05
+	sfx_triangle_instrument $2b
+	sfx_triangle_fraction $05
+	sfx_triangle_note C_2
+	sfx_noise_command_flags $0d
+	sfx_noise_command_flags_bits = $0d
+	sfx_noise_instrument $2b
+	sfx_noise_volume $c
+	sfx_noise_pitch_slide $54
+	sfx_noise_note 9
+
+;2
+	sfx_global_flags $2
+	sfx_global_flags_bits = $2
+	sfx_global_note_sustain_length $ff
+	sfx_frames $05
+	sfx_channel_flags $c
+	sfx_channel_flags_bits = $c
+	sfx_triangle_command_flags $0d
+	sfx_triangle_command_flags_bits = $0d
+	sfx_triangle_instrument $44
+	sfx_triangle_fraction $06
+	sfx_triangle_pitch_slide $7f
+	sfx_triangle_note D_3
+	sfx_noise_command_flags $0d
+	sfx_noise_command_flags_bits = $0d
+	sfx_noise_instrument $44
+	sfx_noise_volume $f
+	sfx_noise_pitch_slide $7f
+	sfx_noise_note 14
+	sfx_end
