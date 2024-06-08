6cd54f04c0cfd6631313d1349474cc721df0f867
lsmmega <pogmegalsm@outlook.com>
2024-06-08 20:07:18 +0800

    Create landing.asm

---
+++ audio/sfx/landing.asm
@@ -0,0 +1,39 @@
+sfx_landing:
+;1
+	sfx_priority $06
+	sfx_global_flags $2
+	sfx_global_flags_bits = $2
+	sfx_global_note_sustain_length $6f
+	sfx_frames $03
+	sfx_channel_flags $a
+	sfx_channel_flags_bits = $a
+	sfx_pulse2_command_flags $1f
+	sfx_pulse2_command_flags_bits = $1f
+	sfx_pulse2_instrument $32
+	sfx_pulse2_duty_cycle 1
+	sfx_pulse2_volume $f
+	sfx_pulse2_pitch_slide $90
+	sfx_pulse2_pitch_tune $08
+	sfx_pulse2_note E_5
+	sfx_noise_command_flags $1d
+	sfx_noise_command_flags_bits = $1d
+	sfx_noise_instrument $31
+	sfx_noise_volume $f
+	sfx_noise_pitch_slide $2a
+	sfx_noise_pitch_tune $7f
+	sfx_noise_note 15
+
+;2
+	sfx_global_flags $2
+	sfx_global_flags_bits = $2
+	sfx_global_note_sustain_length $82
+	sfx_frames $02
+	sfx_channel_flags $a
+	sfx_channel_flags_bits = $a
+	sfx_pulse2_command_flags $00
+	sfx_pulse2_command_flags_bits = $00
+	sfx_pulse2_note E_5
+	sfx_noise_command_flags $00
+	sfx_noise_command_flags_bits = $00
+	sfx_noise_note 15
+	sfx_end
