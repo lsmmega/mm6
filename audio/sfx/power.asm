f98317a0c39f9a033f61d31e13c8347289f6ed3c
lsmmega <164604304+lsmmega@users.noreply.github.com>
2024-06-20 12:39:11 +0800

    Update sfx

---
+++ audio/sfx/power.asm
@@ -0,0 +1,30 @@
+sfx_power:
+;1
+	sfx_priority $07
+	sfx_global_flags $2
+	sfx_global_flags_bits = $2
+	sfx_global_note_sustain_length $ff
+	sfx_global_transpose_note = $00
+	sfx_frames $0c
+	sfx_channel_flags $8
+	sfx_channel_flags_bits = $8
+	sfx_noise_command_flags $0d
+	sfx_noise_command_flags_bits = $0d
+	sfx_noise_instrument $25
+	sfx_noise_volume $f
+	sfx_noise_pitch_slide $d4
+	sfx_noise_note 4
+
+;2
+	sfx_global_flags $0
+	sfx_global_flags_bits = $0
+	sfx_global_transpose_note = $00
+	sfx_frames $09
+	sfx_channel_flags $8
+	sfx_channel_flags_bits = $8
+	sfx_noise_command_flags $0c
+	sfx_noise_command_flags_bits = $0c
+	sfx_noise_volume $c
+	sfx_noise_pitch_slide $78
+	sfx_noise_note 0
+	sfx_end
