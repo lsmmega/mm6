46796c35b3b097eac62ec84b3dd56bb575a50272
lsmmega <164604304+lsmmega@users.noreply.github.com>
2024-06-21 15:12:24 +0800

    Update sfx

---
+++ audio/sfx/yoku_block.asm
@@ -0,0 +1,30 @@
+sfx_yoku_block:
+;1
+	sfx_priority $08
+	sfx_global_flags $2
+	sfx_global_flags_bits = $2
+	sfx_global_note_sustain_length $ff
+	sfx_global_transpose_note = $00
+	sfx_frames $2f
+	sfx_channel_flags $e
+	sfx_channel_flags_bits = $e
+	sfx_pulse2_command_flags $0f
+	sfx_pulse2_command_flags_bits = $0f
+	sfx_pulse2_instrument $28
+	sfx_pulse2_duty_cycle 2
+	sfx_pulse2_volume $f
+	sfx_pulse2_pitch_slide $87
+	sfx_pulse2_note G#2
+	sfx_triangle_command_flags $0d
+	sfx_triangle_command_flags_bits = $0d
+	sfx_triangle_instrument $28
+	sfx_triangle_fraction $ff
+	sfx_triangle_pitch_slide $87
+	sfx_triangle_note G#3
+	sfx_noise_command_flags $0d
+	sfx_noise_command_flags_bits = $0d
+	sfx_noise_instrument $28
+	sfx_noise_volume $f
+	sfx_noise_pitch_slide $88
+	sfx_noise_note 11
+	sfx_end
