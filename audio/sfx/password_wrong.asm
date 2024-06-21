b07964ff55be38f395d27d16ac2925cf01f4c696
lsmmega <164604304+lsmmega@users.noreply.github.com>
2024-06-21 14:43:26 +0800

    Update sfx

---
+++ audio/sfx/password_wrong.asm
@@ -0,0 +1,23 @@
+sfx_password_wrong:
+;1
+	sfx_priority $10
+	sfx_global_flags $2
+	sfx_global_flags_bits = $2
+	sfx_global_note_sustain_length $ff
+	sfx_global_transpose_note = $00
+	sfx_frames $1f
+	sfx_channel_flags $6
+	sfx_channel_flags_bits = $6
+	sfx_pulse2_command_flags $07
+	sfx_pulse2_command_flags_bits = $07
+	sfx_pulse2_instrument $28
+	sfx_pulse2_duty_cycle 1
+	sfx_pulse2_volume $f
+	sfx_pulse2_note D#2
+	sfx_triangle_command_flags $15
+	sfx_triangle_command_flags_bits = $15
+	sfx_triangle_instrument $0e
+	sfx_triangle_fraction $ff
+	sfx_triangle_pitch_tune $fc
+	sfx_triangle_note B_2
+	sfx_end
