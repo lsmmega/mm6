07d9b0cd1fc39d38e7a5239566e5796e235ff156
lsmmega <164604304+lsmmega@users.noreply.github.com>
2024-06-21 13:10:58 +0800

    Update sfx

---
+++ audio/sfx/beat.asm
@@ -0,0 +1,32 @@
+sfx_beat:
+;1
+	sfx_priority $08
+	sfx_global_flags $2
+	sfx_global_flags_bits = $2
+	sfx_global_note_sustain_length $ff
+	sfx_global_transpose_note = $00
+	sfx_frames $0e
+	sfx_channel_flags $2
+	sfx_channel_flags_bits = $2
+	sfx_pulse2_command_flags $0f
+	sfx_pulse2_command_flags_bits = $0f
+	sfx_pulse2_instrument $24
+	sfx_pulse2_duty_cycle 2
+	sfx_pulse2_volume $f
+	sfx_pulse2_pitch_slide $1b
+	sfx_pulse2_note D#6
+
+;2
+	sfx_global_flags $2
+	sfx_global_flags_bits = $2
+	sfx_global_note_sustain_length $7f
+	sfx_global_transpose_note = $00
+	sfx_frames $20
+	sfx_channel_flags $2
+	sfx_channel_flags_bits = $2
+	sfx_pulse2_command_flags $0c
+	sfx_pulse2_command_flags_bits = $0c
+	sfx_pulse2_volume $c
+	sfx_pulse2_pitch_slide $cc
+	sfx_pulse2_note A_5
+	sfx_end
