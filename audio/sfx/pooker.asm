1ae183488c660fc4348914fd5a841adbe3586bcd
lsmmega <164604304+lsmmega@users.noreply.github.com>
2024-06-22 14:21:51 +0800

    Update sfx

---
+++ audio/sfx/pooker.asm
@@ -0,0 +1,43 @@
+sfx_pooker:
+;1
+	sfx_priority $07
+	sfx_global_flags $2
+	sfx_global_flags_bits = $2
+	sfx_global_note_sustain_length $ff
+	sfx_global_transpose_note = $00
+	sfx_frames $05
+	sfx_channel_flags $2
+	sfx_channel_flags_bits = $2
+	sfx_pulse2_command_flags $0f
+	sfx_pulse2_command_flags_bits = $0f
+	sfx_pulse2_instrument $28
+	sfx_pulse2_duty_cycle 2
+	sfx_pulse2_volume $c
+	sfx_pulse2_pitch_slide $a7
+	sfx_pulse2_note E_4
+
+;2
+	sfx_global_flags $0
+	sfx_global_flags_bits = $0
+	sfx_global_transpose_note = $00
+	sfx_frames $08
+	sfx_channel_flags $2
+	sfx_channel_flags_bits = $2
+	sfx_pulse2_command_flags $0c
+	sfx_pulse2_command_flags_bits = $0c
+	sfx_pulse2_volume $e
+	sfx_pulse2_pitch_slide $4c
+	sfx_pulse2_note $ff
+
+;3
+	sfx_global_flags $0
+	sfx_global_flags_bits = $0
+	sfx_global_transpose_note = $00
+	sfx_frames $08
+	sfx_channel_flags $2
+	sfx_channel_flags_bits = $2
+	sfx_pulse2_command_flags $04
+	sfx_pulse2_command_flags_bits = $04
+	sfx_pulse2_volume $7
+	sfx_pulse2_note $ff
+	sfx_end
