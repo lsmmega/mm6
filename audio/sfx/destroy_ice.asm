93a6e337ec55dbe1784ac24bfe42627aa143facf
lsmmega <164604304+lsmmega@users.noreply.github.com>
2024-06-22 08:39:00 +0800

    Update sfx

---
+++ audio/sfx/destroy_ice.asm
@@ -0,0 +1,86 @@
+sfx_destroy_ice:
+;1
+	sfx_priority $08
+	sfx_global_flags $2
+	sfx_global_flags_bits = $2
+	sfx_global_note_sustain_length $aa
+	sfx_global_transpose_note = $00
+	sfx_frames $03
+	sfx_channel_flags $a
+	sfx_channel_flags_bits = $a
+	sfx_pulse2_command_flags $0d
+	sfx_pulse2_command_flags_bits = $0d
+	sfx_pulse2_instrument $34
+	sfx_pulse2_volume $f
+	sfx_pulse2_pitch_slide $ff
+	sfx_pulse2_note C_6
+	sfx_noise_command_flags $0d
+	sfx_noise_command_flags_bits = $0d
+	sfx_noise_instrument $28
+	sfx_noise_volume $f
+	sfx_noise_pitch_slide $b7
+	sfx_noise_note 9
+
+;2
+	sfx_global_flags $0
+	sfx_global_flags_bits = $0
+	sfx_global_transpose_note = $00
+	sfx_frames $02
+	sfx_channel_flags $a
+	sfx_channel_flags_bits = $a
+	sfx_pulse2_command_flags $00
+	sfx_pulse2_command_flags_bits = $00
+	sfx_pulse2_note E_6
+	sfx_noise_command_flags $00
+	sfx_noise_command_flags_bits = $00
+	sfx_noise_note 11
+
+;3
+	sfx_global_flags $0
+	sfx_global_flags_bits = $0
+	sfx_global_transpose_note = $00
+	sfx_frames $02
+	sfx_channel_flags $a
+	sfx_channel_flags_bits = $a
+	sfx_pulse2_command_flags $00
+	sfx_pulse2_command_flags_bits = $00
+	sfx_pulse2_note A_6
+	sfx_noise_command_flags $00
+	sfx_noise_command_flags_bits = $00
+	sfx_noise_note 14
+
+;4
+	sfx_global_flags $0
+	sfx_global_flags_bits = $0
+	sfx_global_transpose_note = $00
+	sfx_frames $01
+	sfx_channel_flags $a
+	sfx_channel_flags_bits = $a
+	sfx_pulse2_command_flags $00
+	sfx_pulse2_command_flags_bits = $00
+	sfx_pulse2_note F#6
+	sfx_noise_command_flags $00
+	sfx_noise_command_flags_bits = $00
+	sfx_noise_note 13
+
+;5
+	sfx_global_flags $2
+	sfx_global_flags_bits = $2
+	sfx_global_note_sustain_length $82
+	sfx_global_transpose_note = $00
+	sfx_frames $0e
+	sfx_channel_flags $a
+	sfx_channel_flags_bits = $a
+	sfx_pulse2_command_flags $0d
+	sfx_pulse2_command_flags_bits = $0d
+	sfx_pulse2_instrument $38
+	sfx_pulse2_volume $7
+	sfx_pulse2_pitch_slide $32
+	sfx_pulse2_note D_6
+	sfx_noise_command_flags $0d
+	sfx_noise_command_flags_bits = $0d
+	sfx_noise_instrument $3a
+	sfx_noise_volume $a
+	sfx_noise_pitch_slide $59
+	sfx_noise_note 11
+	sfx_end
