2dfab40ecf36f25ce14e5905a3767740e23941c5
lsmmega <164604304+lsmmega@users.noreply.github.com>
2024-06-22 11:22:11 +0800

    Update sfx

---
+++ audio/sfx/ship_down.asm
@@ -0,0 +1,29 @@
+sfx_ship_down:
+;1
+	sfx_priority $07
+	sfx_global_flags $2
+	sfx_global_flags_bits = $2
+	sfx_global_note_sustain_length $b5
+	sfx_global_transpose_note = $00
+	sfx_frames $4b
+	sfx_channel_flags $8
+	sfx_channel_flags_bits = $8
+	sfx_noise_command_flags $05
+	sfx_noise_command_flags_bits = $05
+	sfx_noise_instrument $14
+	sfx_noise_volume $f
+	sfx_noise_note 7
+
+;2
+	sfx_global_flags $2
+	sfx_global_flags_bits = $2
+	sfx_global_note_sustain_length $09
+	sfx_global_transpose_note = $00
+	sfx_frames $13
+	sfx_channel_flags $8
+	sfx_channel_flags_bits = $8
+	sfx_noise_command_flags $04
+	sfx_noise_command_flags_bits = $04
+	sfx_noise_volume $c
+	sfx_noise_note 10
+	sfx_end
