9b5500713c0f2fee2404d324c33b07a478214526
lsmmega <164604304+lsmmega@users.noreply.github.com>
2024-06-22 08:21:36 +0800

    Update sfx

---
+++ audio/sfx/unused3d.asm
@@ -0,0 +1,39 @@
+sfx_unused3d:
+;1
+	sfx_priority $07
+	sfx_global_flags $2
+	sfx_global_flags_bits = $2
+	sfx_global_note_sustain_length $0b
+	sfx_global_transpose_note = $00
+	sfx_frames $02
+	sfx_channel_flags $8
+	sfx_channel_flags_bits = $8
+	sfx_noise_command_flags $05
+	sfx_noise_command_flags_bits = $05
+	sfx_noise_instrument $39
+	sfx_noise_volume $f
+	sfx_noise_note 5
+
+;2
+	sfx_global_flags $2
+	sfx_global_flags_bits = $2
+	sfx_global_note_sustain_length $64
+	sfx_global_transpose_note = $00
+	sfx_frames $02
+	sfx_channel_flags $8
+	sfx_channel_flags_bits = $8
+	sfx_noise_command_flags $00
+	sfx_noise_command_flags_bits = $00
+	sfx_noise_note 8
+
+;3
+	sfx_global_flags $0
+	sfx_global_flags_bits = $0
+	sfx_global_transpose_note = $00
+	sfx_frames $01
+	sfx_channel_flags $8
+	sfx_channel_flags_bits = $8
+	sfx_noise_command_flags $00
+	sfx_noise_command_flags_bits = $00
+	sfx_noise_note $00
+	sfx_end
