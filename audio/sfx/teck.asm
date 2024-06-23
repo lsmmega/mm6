5023cb17cc6bb9a9cb1ed2ee4f148e736d4eec55
lsmmega <164604304+lsmmega@users.noreply.github.com>
2024-06-23 11:18:50 +0800

    Update sfx

---
+++ audio/sfx/teck.asm
@@ -0,0 +1,16 @@
+sfx_teck:
+;1
+	sfx_priority $07
+	sfx_global_flags $2
+	sfx_global_flags_bits = $2
+	sfx_global_note_sustain_length $ff
+	sfx_global_transpose_note = $00
+	sfx_frames $1c
+	sfx_channel_flags $4
+	sfx_channel_flags_bits = $4
+	sfx_triangle_command_flags $05
+	sfx_triangle_command_flags_bits = $05
+	sfx_triangle_instrument $2b
+	sfx_triangle_fraction $5b
+	sfx_triangle_note C_5
+	sfx_end
