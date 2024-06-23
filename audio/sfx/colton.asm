f63622f278e3b5aefe679fc8e69ab13dab1e1ae8
lsmmega <164604304+lsmmega@users.noreply.github.com>
2024-06-23 11:10:25 +0800

    Update sfx

---
+++ audio/sfx/colton.asm
@@ -0,0 +1,32 @@
+sfx_colton:
+;1
+	sfx_priority $07
+	sfx_global_flags $2
+	sfx_global_flags_bits = $2
+	sfx_global_note_sustain_length $ff
+	sfx_global_transpose_note = $00
+	sfx_frames $03
+	sfx_channel_flags $8
+	sfx_channel_flags_bits = $8
+	sfx_noise_command_flags $0d
+	sfx_noise_command_flags_bits = $0d
+	sfx_noise_instrument $28
+	sfx_noise_volume $f
+	sfx_noise_pitch_slide $ff
+	sfx_noise_note 1
+
+;2
+	sfx_global_flags $2
+	sfx_global_flags_bits = $2
+	sfx_global_note_sustain_length $a8
+	sfx_global_transpose_note = $00
+	sfx_frames $1e
+	sfx_channel_flags $8
+	sfx_channel_flags_bits = $8
+	sfx_noise_command_flags $0d
+	sfx_noise_command_flags_bits = $0d
+	sfx_noise_instrument $3a
+	sfx_noise_volume $e
+	sfx_noise_pitch_slide $36
+	sfx_noise_note 12
+	sfx_end
