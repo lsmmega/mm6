94e6eeda3288ccb5510a2ec799856cc8d408fa0b
lsmmega <164604304+lsmmega@users.noreply.github.com>
2024-06-23 11:14:30 +0800

    Update sfx

---
+++ audio/sfx/fire_telly.asm
@@ -0,0 +1,22 @@
+sfx_fire_telly:
+;1
+	sfx_priority $07
+	sfx_global_flags $2
+	sfx_global_flags_bits = $2
+	sfx_global_note_sustain_length $af
+	sfx_global_transpose_note = $00
+	sfx_frames $06
+	sfx_channel_flags $a
+	sfx_channel_flags_bits = $a
+	sfx_pulse2_command_flags $07
+	sfx_pulse2_command_flags_bits = $07
+	sfx_pulse2_instrument $36
+	sfx_pulse2_duty_cycle 2
+	sfx_pulse2_volume $f
+	sfx_pulse2_note F_2
+	sfx_noise_command_flags $05
+	sfx_noise_command_flags_bits = $05
+	sfx_noise_instrument $36
+	sfx_noise_volume $f
+	sfx_noise_note 2
+	sfx_end
