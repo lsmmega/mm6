1d752a3b5e2aa186078bac889f94e0b410b8e48a
lsmmega <164604304+lsmmega@users.noreply.github.com>
2024-06-23 11:22:59 +0800

    Update sfx

---
+++ audio/sfx/curlinger.asm
@@ -0,0 +1,19 @@
+sfx_curlinger:
+;1
+	sfx_priority $06
+	sfx_global_flags $2
+	sfx_global_flags_bits = $2
+	sfx_global_note_sustain_length $01
+	sfx_global_transpose_note = $00
+	sfx_frames $0a
+	sfx_channel_flags $2
+	sfx_channel_flags_bits = $2
+	sfx_pulse2_command_flags $1f
+	sfx_pulse2_command_flags_bits = $1f
+	sfx_pulse2_instrument $3a
+	sfx_pulse2_duty_cycle 2
+	sfx_pulse2_volume $d
+	sfx_pulse2_pitch_slide $01
+	sfx_pulse2_pitch_tune $07
+	sfx_pulse2_note A_4
+	sfx_end
