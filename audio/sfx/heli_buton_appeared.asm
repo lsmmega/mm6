9d2ee893471111a04ae433d7f80809c9be91938a
lsmmega <164604304+lsmmega@users.noreply.github.com>
2024-06-24 12:35:09 +0800

    Update sfx

---
+++ audio/sfx/heli_buton_appeared.asm
@@ -0,0 +1,28 @@
+sfx_heli_buton_appeared:
+;1
+	sfx_priority $07
+	sfx_global_flags $2
+	sfx_global_flags_bits = $2
+	sfx_global_note_sustain_length $9d
+	sfx_global_transpose_note = $00
+	sfx_frames $01
+	sfx_channel_flags $8
+	sfx_channel_flags_bits = $8
+	sfx_noise_command_flags $05
+	sfx_noise_command_flags_bits = $05
+	sfx_noise_instrument $22
+	sfx_noise_volume $f
+	sfx_noise_note 3
+
+;2
+	sfx_global_flags $0
+	sfx_global_flags_bits = $0
+	sfx_global_transpose_note = $00
+	sfx_frames $05
+	sfx_channel_flags $8
+	sfx_channel_flags_bits = $8
+	sfx_noise_command_flags $08
+	sfx_noise_command_flags_bits = $08
+	sfx_noise_pitch_slide $55
+	sfx_noise_note 8
+	sfx_end
