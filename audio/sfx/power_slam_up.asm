e360c4e838a1e646ccca7fbbe789e2c860061ba6
lsmmega <164604304+lsmmega@users.noreply.github.com>
2024-06-24 08:12:02 +0800

    Update sfx

---
+++ audio/sfx/power_slam_up.asm
@@ -0,0 +1,30 @@
+sfx_power_slam_up:
+;1
+	sfx_priority $07
+	sfx_global_flags $2
+	sfx_global_flags_bits = $2
+	sfx_global_note_sustain_length $ff
+	sfx_global_transpose_note = $00
+	sfx_frames $03
+	sfx_channel_flags $8
+	sfx_channel_flags_bits = $8
+	sfx_noise_command_flags $05
+	sfx_noise_command_flags_bits = $05
+	sfx_noise_instrument $28
+	sfx_noise_volume $f
+	sfx_noise_note 3
+
+;2
+	sfx_global_flags $2
+	sfx_global_flags_bits = $2
+	sfx_global_note_sustain_length $bd
+	sfx_global_transpose_note = $00
+	sfx_frames $0e
+	sfx_channel_flags $8
+	sfx_channel_flags_bits = $8
+	sfx_noise_command_flags $09
+	sfx_noise_command_flags_bits = $09
+	sfx_noise_instrument $3f
+	sfx_noise_pitch_slide $3d
+	sfx_noise_note 7
+	sfx_end
