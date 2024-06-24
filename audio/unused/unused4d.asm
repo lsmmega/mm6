8fdf3fd80adbc07b0c51f74677c610befbb3ea03
lsmmega <164604304+lsmmega@users.noreply.github.com>
2024-06-24 10:20:13 +0800

    Update unused

---
+++ audio/unused/unused4d.asm
@@ -0,0 +1,36 @@
+sfx_unused4d:
+;1
+	sfx_priority $07
+	sfx_global_flags $2
+	sfx_global_flags_bits = $2
+	sfx_global_note_sustain_length $ff
+	sfx_global_transpose_note = $00
+	sfx_frames $02
+	sfx_channel_flags $2
+	sfx_channel_flags_bits = $2
+	sfx_pulse2_command_flags $05
+	sfx_pulse2_command_flags_bits = $05
+	sfx_pulse2_instrument $1c
+	sfx_pulse2_volume $f
+	sfx_pulse2_note D#3
+
+;2
+	sfx_global_flags $2
+	sfx_global_flags_bits = $2
+	sfx_global_note_sustain_length $01
+	sfx_global_transpose_note = $00
+	sfx_frames $06
+	sfx_channel_flags $a
+	sfx_channel_flags_bits = $a
+	sfx_pulse2_command_flags $15
+	sfx_pulse2_command_flags_bits = $15
+	sfx_pulse2_instrument $37
+	sfx_pulse2_volume $d
+	sfx_pulse2_pitch_tune $fc
+	sfx_pulse2_note D#3
+	sfx_noise_command_flags $05
+	sfx_noise_command_flags_bits = $05
+	sfx_noise_instrument $37
+	sfx_noise_volume $6
+	sfx_noise_note 10
+	sfx_end
