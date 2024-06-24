56a1fda1d8dc911a16988e8728bd1c6704ee9681
lsmmega <164604304+lsmmega@users.noreply.github.com>
2024-06-24 12:31:42 +0800

    Update sfx

---
+++ audio/sfx/flamethrower.asm
@@ -0,0 +1,30 @@
+sfx_flamethrower:
+;1
+	sfx_priority $07
+	sfx_global_flags $2
+	sfx_global_flags_bits = $2
+	sfx_global_note_sustain_length $ff
+	sfx_global_transpose_note = $00
+	sfx_frames $08
+	sfx_channel_flags $8
+	sfx_channel_flags_bits = $8
+	sfx_noise_command_flags $0d
+	sfx_noise_command_flags_bits = $0d
+	sfx_noise_instrument $20
+	sfx_noise_volume $f
+	sfx_noise_pitch_slide $90
+	sfx_noise_note 1
+
+;2
+	sfx_global_flags $2
+	sfx_global_flags_bits = $2
+	sfx_global_note_sustain_length $aa
+	sfx_global_transpose_note = $00
+	sfx_frames $1b
+	sfx_channel_flags $8
+	sfx_channel_flags_bits = $8
+	sfx_noise_command_flags $08
+	sfx_noise_command_flags_bits = $08
+	sfx_noise_pitch_slide $07
+	sfx_noise_note 4
+	sfx_end
