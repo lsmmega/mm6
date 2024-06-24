57f1ea60a84ab1926f6036299f9b3db01343fd64
lsmmega <164604304+lsmmega@users.noreply.github.com>
2024-06-24 10:14:45 +0800

    Update sfx

---
+++ audio/sfx/skull_walker.asm
@@ -0,0 +1,28 @@
+sfx_skull_walker:
+;1
+	sfx_priority $07
+	sfx_global_flags $2
+	sfx_global_flags_bits = $2
+	sfx_global_note_sustain_length $c3
+	sfx_global_transpose_note = $00
+	sfx_frames $02
+	sfx_channel_flags $8
+	sfx_channel_flags_bits = $8
+	sfx_noise_command_flags $05
+	sfx_noise_command_flags_bits = $05
+	sfx_noise_instrument $21
+	sfx_noise_volume $f
+	sfx_noise_note 3
+
+;2
+	sfx_global_flags $2
+	sfx_global_flags_bits = $2
+	sfx_global_note_sustain_length $ff
+	sfx_global_transpose_note = $00
+	sfx_frames $03
+	sfx_channel_flags $8
+	sfx_channel_flags_bits = $8
+	sfx_noise_command_flags $00
+	sfx_noise_command_flags_bits = $00
+	sfx_noise_note 6
+	sfx_end
