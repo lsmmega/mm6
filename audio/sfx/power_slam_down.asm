1a8ae8710720f2de18be117eff2415feca3b8ff6
lsmmega <164604304+lsmmega@users.noreply.github.com>
2024-06-24 09:41:56 +0800

    Update sfx

---
+++ audio/sfx/power_slam_down.asm
@@ -0,0 +1,38 @@
+sfx_power_slam_down:
+;1
+	sfx_priority $07
+	sfx_global_flags $2
+	sfx_global_flags_bits = $2
+	sfx_global_note_sustain_length $5f
+	sfx_global_transpose_note = $00
+	sfx_frames $05
+	sfx_channel_flags $a
+	sfx_channel_flags_bits = $a
+	sfx_pulse2_command_flags $05
+	sfx_pulse2_command_flags_bits = $05
+	sfx_pulse2_instrument $40
+	sfx_pulse2_volume $f
+	sfx_pulse2_note C_4
+	sfx_noise_command_flags $05
+	sfx_noise_command_flags_bits = $05
+	sfx_noise_instrument $30
+	sfx_noise_volume $f
+	sfx_noise_note 7
+
+;2
+	sfx_global_flags $2
+	sfx_global_flags_bits = $2
+	sfx_global_note_sustain_length $e4
+	sfx_global_transpose_note = $00
+	sfx_frames $07
+	sfx_channel_flags $a
+	sfx_channel_flags_bits = $a
+	sfx_pulse2_command_flags $08
+	sfx_pulse2_command_flags_bits = $08
+	sfx_pulse2_pitch_slide $55
+	sfx_pulse2_note F#4
+	sfx_noise_command_flags $08
+	sfx_noise_command_flags_bits = $08
+	sfx_noise_pitch_slide $5a
+	sfx_noise_note 9
+	sfx_end
