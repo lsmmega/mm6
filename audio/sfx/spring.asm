0825ec12c73f5c450701e93f8a8c9a43d9b4f6f0
lsmmega <164604304+lsmmega@users.noreply.github.com>
2024-06-24 13:23:22 +0800

    Update sfx

---
+++ audio/sfx/spring.asm
@@ -0,0 +1,17 @@
+sfx_spring:
+;1
+	sfx_priority $07
+	sfx_global_flags $2
+	sfx_global_flags_bits = $2
+	sfx_global_note_sustain_length $ff
+	sfx_global_transpose_note = $00
+	sfx_frames $12
+	sfx_channel_flags $2
+	sfx_channel_flags_bits = $2
+	sfx_pulse2_command_flags $07
+	sfx_pulse2_command_flags_bits = $07
+	sfx_pulse2_instrument $38
+	sfx_pulse2_duty_cycle 2
+	sfx_pulse2_volume $f
+	sfx_pulse2_note C_3
+	sfx_end
