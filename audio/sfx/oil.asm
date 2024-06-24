f0a926254b7bc52f512bc4d12aa283201c5d8f9d
lsmmega <164604304+lsmmega@users.noreply.github.com>
2024-06-24 11:04:14 +0800

    Update sfx

---
+++ audio/sfx/oil.asm
@@ -0,0 +1,44 @@
+sfx_oil:
+;1
+	sfx_priority $07
+	sfx_global_flags $2
+	sfx_global_flags_bits = $2
+	sfx_global_note_sustain_length $ff
+	sfx_global_transpose_note = $00
+	sfx_frames $03
+	sfx_channel_flags $2
+	sfx_channel_flags_bits = $2
+	sfx_pulse2_command_flags $07
+	sfx_pulse2_command_flags_bits = $07
+	sfx_pulse2_instrument $28
+	sfx_pulse2_duty_cycle 2
+	sfx_pulse2_volume $f
+	sfx_pulse2_note C#4
+
+;2
+	sfx_global_flags $0
+	sfx_global_flags_bits = $0
+	sfx_global_transpose_note = $00
+	sfx_frames $05
+	sfx_channel_flags $0
+	sfx_channel_flags_bits = $0
+
+;3
+	sfx_global_flags $0
+	sfx_global_flags_bits = $0
+	sfx_global_transpose_note = $00
+	sfx_frames $07
+	sfx_channel_flags $6
+	sfx_channel_flags_bits = $6
+	sfx_pulse2_command_flags $09
+	sfx_pulse2_command_flags_bits = $09
+	sfx_pulse2_instrument $06
+	sfx_pulse2_pitch_slide $c1
+	sfx_pulse2_note C_4
+	sfx_triangle_command_flags $0d
+	sfx_triangle_command_flags_bits = $0d
+	sfx_triangle_instrument $06
+	sfx_triangle_fraction $c3
+	sfx_triangle_pitch_slide $c1
+	sfx_triangle_note D#5
+	sfx_end
