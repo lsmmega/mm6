3744c52e60162a3606b1e3d84816438cb7b2546e
lsmmega <164604304+lsmmega@users.noreply.github.com>
2024-06-24 08:15:54 +0800

    Update sfx

---
+++ audio/sfx/enemy_falling.asm
@@ -0,0 +1,18 @@
+sfx_enemy_falling:
+;1
+	sfx_priority $06
+	sfx_global_flags $2
+	sfx_global_flags_bits = $2
+	sfx_global_note_sustain_length $ff
+	sfx_global_transpose_note = $00
+	sfx_frames $2d
+	sfx_channel_flags $2
+	sfx_channel_flags_bits = $2
+	sfx_pulse2_command_flags $0f
+	sfx_pulse2_command_flags_bits = $0f
+	sfx_pulse2_instrument $28
+	sfx_pulse2_duty_cycle 2
+	sfx_pulse2_volume $9
+	sfx_pulse2_pitch_slide $28
+	sfx_pulse2_note A_5
+	sfx_end
