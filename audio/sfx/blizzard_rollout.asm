6e86e1aabac83ccf97ce1ebbd9f8f72b27527d5b
lsmmega <164604304+lsmmega@users.noreply.github.com>
2024-06-25 09:50:58 +0800

    Update sfx

---
+++ audio/sfx/blizzard_rollout.asm
@@ -0,0 +1,42 @@
+sfx_blizzard_rollout:
+;1
+	sfx_priority $07
+	sfx_global_flags $2
+	sfx_global_flags_bits = $2
+	sfx_global_note_sustain_length $ff
+	sfx_global_transpose_note = $00
+	sfx_frames $05
+	sfx_channel_flags $a
+	sfx_channel_flags_bits = $a
+	sfx_pulse2_command_flags $0f
+	sfx_pulse2_command_flags_bits = $0f
+	sfx_pulse2_instrument $37
+	sfx_pulse2_duty_cycle 2
+	sfx_pulse2_volume $f
+	sfx_pulse2_pitch_slide $ff
+	sfx_pulse2_note A_1
+	sfx_noise_command_flags $0d
+	sfx_noise_command_flags_bits = $0d
+	sfx_noise_instrument $37
+	sfx_noise_volume $a
+	sfx_noise_pitch_slide $ff
+	sfx_noise_note 3
+
+;2
+	sfx_global_flags $2
+	sfx_global_flags_bits = $2
+	sfx_global_note_sustain_length $9a
+	sfx_global_transpose_note = $00
+	sfx_frames $17
+	sfx_channel_flags $a
+	sfx_channel_flags_bits = $a
+	sfx_pulse2_command_flags $0c
+	sfx_pulse2_command_flags_bits = $0c
+	sfx_pulse2_volume $d
+	sfx_pulse2_pitch_slide $37
+	sfx_pulse2_note_sustain
+	sfx_noise_command_flags $08
+	sfx_noise_command_flags_bits = $08
+	sfx_noise_pitch_slide $21
+	sfx_noise_note_sustain
+	sfx_end
