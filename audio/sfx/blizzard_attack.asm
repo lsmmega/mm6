9371c92fafd082a37329f14fca3b513df6c5fc3b
lsmmega <164604304+lsmmega@users.noreply.github.com>
2024-06-25 09:26:01 +0800

    Update sfx

---
+++ audio/sfx/blizzard_attack.asm
@@ -0,0 +1,64 @@
+sfx_blizzard_attack:
+;1
+	sfx_priority $09
+	sfx_global_flags $2
+	sfx_global_flags_bits = $2
+	sfx_global_note_sustain_length $ff
+	sfx_global_transpose_note = $00
+	sfx_frames $03
+	sfx_channel_flags $a
+	sfx_channel_flags_bits = $a
+	sfx_pulse2_command_flags $05
+	sfx_pulse2_command_flags_bits = $05
+	sfx_pulse2_instrument $3a
+	sfx_pulse2_volume $f
+	sfx_pulse2_note G_5
+	sfx_noise_command_flags $05
+	sfx_noise_command_flags_bits = $05
+	sfx_noise_instrument $3a
+	sfx_noise_volume $8
+	sfx_noise_note 10
+
+;2
+	sfx_global_flags $0
+	sfx_global_flags_bits = $0
+	sfx_global_transpose_note = $00
+	sfx_frames $01
+	sfx_channel_flags $a
+	sfx_channel_flags_bits = $a
+	sfx_pulse2_command_flags $00
+	sfx_pulse2_command_flags_bits = $00
+	sfx_pulse2_note B_5
+	sfx_noise_command_flags $00
+	sfx_noise_command_flags_bits = $00
+	sfx_noise_note 13
+
+;3
+	sfx_global_flags $0
+	sfx_global_flags_bits = $0
+	sfx_global_transpose_note = $00
+	sfx_frames $01
+	sfx_channel_flags $a
+	sfx_channel_flags_bits = $a
+	sfx_pulse2_command_flags $00
+	sfx_pulse2_command_flags_bits = $00
+	sfx_pulse2_note E_6_
+	sfx_noise_command_flags $00
+	sfx_noise_command_flags_bits = $00
+	sfx_noise_note 11
+
+;4
+	sfx_global_flags $2
+	sfx_global_flags_bits = $2
+	sfx_global_note_sustain_length $26
+	sfx_global_transpose_note = $00
+	sfx_frames $0c
+	sfx_channel_flags $a
+	sfx_channel_flags_bits = $a
+	sfx_pulse2_command_flags $00
+	sfx_pulse2_command_flags_bits = $00
+	sfx_pulse2_note A_6
+	sfx_noise_command_flags $00
+	sfx_noise_command_flags_bits = $00
+	sfx_noise_note 14
+	sfx_end
