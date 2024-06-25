1716049c61f6751beca611034771c6e5542024b9
lsmmega <164604304+lsmmega@users.noreply.github.com>
2024-06-25 10:06:10 +0800

    Update sfx

---
+++ audio/sfx/centaur_disappeared.asm
@@ -0,0 +1,76 @@
+sfx_centaur_disappeared1:
+;1
+	sfx_priority $07
+	sfx_global_flags $2
+	sfx_global_flags_bits = $2
+	sfx_global_note_sustain_length $02
+	sfx_global_transpose_note = $00
+	sfx_frames $03
+	sfx_channel_flags $2
+	sfx_channel_flags_bits = $2
+	sfx_pulse2_command_flags $0f
+	sfx_pulse2_command_flags_bits = $0f
+	sfx_pulse2_instrument $1e
+	sfx_pulse2_duty_cycle 2
+	sfx_pulse2_volume $f
+	sfx_pulse2_pitch_slide $18
+	sfx_pulse2_note G#2
+
+;2
+	sfx_global_flags $0
+	sfx_global_flags_bits = $0
+	sfx_global_transpose_note = $00
+	sfx_frames $03
+	sfx_channel_flags $2
+	sfx_channel_flags_bits = $2
+	sfx_pulse2_command_flags $00
+	sfx_pulse2_command_flags_bits = $00
+	sfx_pulse2_note C_3
+
+;3
+	sfx_global_flags $0
+	sfx_global_flags_bits = $0
+	sfx_global_transpose_note = $00
+	sfx_frames $03
+	sfx_channel_flags $2
+	sfx_channel_flags_bits = $2
+	sfx_pulse2_command_flags $00
+	sfx_pulse2_command_flags_bits = $00
+	sfx_pulse2_note F_3
+
+;4
+	sfx_global_flags $0
+	sfx_global_flags_bits = $0
+	sfx_global_transpose_note = $00
+	sfx_frames $03
+	sfx_channel_flags $2
+	sfx_channel_flags_bits = $2
+	sfx_pulse2_command_flags $04
+	sfx_pulse2_command_flags_bits = $04
+	sfx_pulse2_volume $b
+	sfx_pulse2_note G#2
+
+;5
+	sfx_global_flags $0
+	sfx_global_flags_bits = $0
+	sfx_global_transpose_note = $00
+	sfx_frames $03
+	sfx_channel_flags $2
+	sfx_channel_flags_bits = $2
+	sfx_pulse2_command_flags $04
+	sfx_pulse2_command_flags_bits = $04
+	sfx_pulse2_volume $9
+	sfx_pulse2_note B_2
+
+;6
+	sfx_global_flags $0
+	sfx_global_flags_bits = $0
+	sfx_global_transpose_note = $00
+	sfx_frames $05
+	sfx_channel_flags $2
+	sfx_channel_flags_bits = $2
+	sfx_pulse2_command_flags $04
+	sfx_pulse2_command_flags_bits = $04
+	sfx_pulse2_volume $7
+	sfx_pulse2_note F_3
+	sfx_end
