72e5b1bb6150482ea474889d311e0780bc5eef06
lsmmega <164604304+lsmmega@users.noreply.github.com>
2024-06-26 08:35:50 +0800

    Update sfx

---
+++ audio/sfx/wily_machine.asm
@@ -0,0 +1,38 @@
+sfx_wily_machine:
+;1
+	sfx_priority $09
+	sfx_global_flags $2
+	sfx_global_flags_bits = $2
+	sfx_global_note_sustain_length $ff
+	sfx_global_transpose_note = $00
+	sfx_frames $02
+	sfx_channel_flags $a
+	sfx_channel_flags_bits = $a
+	sfx_pulse2_command_flags $07
+	sfx_pulse2_command_flags_bits = $07
+	sfx_pulse2_instrument $46
+	sfx_pulse2_duty_cycle 2
+	sfx_pulse2_volume $f
+	sfx_pulse2_note E_2
+	sfx_noise_command_flags $05
+	sfx_noise_command_flags_bits = $05
+	sfx_noise_instrument $46
+	sfx_noise_volume $f
+	sfx_noise_note 2
+
+;2
+	sfx_global_flags $2
+	sfx_global_flags_bits = $2
+	sfx_global_note_sustain_length $88
+	sfx_global_transpose_note = $00
+	sfx_frames $1c
+	sfx_channel_flags $a
+	sfx_channel_flags_bits = $a
+	sfx_pulse2_command_flags $04
+	sfx_pulse2_command_flags_bits = $04
+	sfx_pulse2_volume $a
+	sfx_pulse2_note F_2
+	sfx_noise_command_flags $00
+	sfx_noise_command_flags_bits = $00
+	sfx_noise_note 4
+	sfx_end
