79c3d169d5068678bc69dd33b60d84b2bccc6159
lsmmega <pogmegalsm@outlook.com>
2024-10-11 20:30:26 +0800

    create small weapon capsule

--- build.bat
+++ build.bat
@@ -2,5 +2,6 @@ bmp2nes gfx/obj/mm/mm.bmp
 bmp2nes gfx/obj/wpn/charge_shot.bmp
 bmp2nes gfx/obj/bar.bmp
 bmp2nes gfx/obj/wpn/mega_buster.bmp
+bmp2nes gfx/obj/capsule/small_wpn_capsule.bmp
 asm6 main.asm mm6.nes
 @pause
