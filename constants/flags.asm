5b858fc0376da83d822c2b2c629fc62435b6ce93
lsmmega <pogmegalsm@outlook.com>
2025-10-13 15:26:07 +0800

    Create flags.asm

---
+++ constants/flags.asm
@@ -0,0 +1,28 @@
+;beat letter flags.
+	got_letter_b = 1 << 2
+	got_letter_e = 1 << 2
+	got_letter_a = 1 << 2
+	got_letter_t = 1 << 2
+
+;energy balancer.
+	got_energy_balancer = 1
+
+;rematch flags.
+	rematch_blizzardman_done = 1 << 0
+	rematch_windman_done     = 1 << 1
+	rematch_plantman_done    = 1 << 2
+	rematch_flameman_done    = 1 << 3
+	rematch_yamatoman_done   = 1 << 4
+	rematch_tomahawkman_done = 1 << 5
+	rematch_knightman_done   = 1 << 6
+	rematch_centaurman_done  = 1 << 7
+
+;regular stage clear flags.
+	stage_blizzardman_clear = 1 << 0
+	stage_windman_clear     = 1 << 0
+	stage_plantman_clear    = 1 << 0
+	stage_flameman_clear    = 1 << 0
+	stage_yamatoman_clear   = 1 << 1
+	stage_tomahawkman_clear = 1 << 1
+	stage_knightman_clear   = 1 << 1
+	stage_centaurman_clear  = 1 << 1
