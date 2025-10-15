519f048e42c1acd93a2232a34935f3ae5b73b520
lsmmega <pogmegalsm@outlook.com>
2025-10-15 19:25:28 +0800

    create audio macro

---
+++ constants/audio.asm
@@ -0,0 +1,105 @@
+;base note index set.
+.ENUM
+	_E_1 ;non-standard note
+	_Gb1 ;non-standard note
+	_A_1 ;non-standard note
+	_Db2 ;non-standard note
+	_E_2 ;non-standard note
+	_A_2 ;non-standard note
+	_D_3 ;non-standard note
+	_Bb3 ;non-standard note
+	_C_5 ;non-standard note
+	A_0
+	Bb0
+	B_0
+	C_1
+	Db1
+	D_1
+	Eb1
+	E_1
+	F_1
+	Gb1
+	G_1
+	Ab1
+	A_1
+	Bb1
+	B_1
+	C_2
+	Db2
+	D_2
+	Eb2
+	E_2
+	F_2
+	Gb2
+	G_2
+	Ab2
+	A_2
+	Bb2
+	B_2
+	C_3
+	Db3
+	D_3
+	Eb3
+	E_3
+	F_3
+	Gb3
+	G_3
+	Ab3
+	A_3
+	Bb3
+	B_3
+	C_4
+	Db4
+	D_4
+	Eb4
+	E_4
+	F_4
+	Gb4
+	G_4
+	Ab4
+	A_4
+	Bb4
+	B_4
+	C_5
+	Db5
+	D_5
+	Eb5
+	E_5
+	F_5
+	Gb5
+	G_5
+	Ab5
+	A_5
+	Bb5
+	B_5
+	C_6
+	Db6
+	D_6
+	Eb6
+	E_6
+	F_6
+	Gb6
+	G_6
+	Ab6
+	A_6
+	Bb6
+	B_6
+	C_7
+	Db7
+	D_7
+	Eb7
+	E_7
+	F_7
+	Gb7
+	G_7
+	Ab7
+	A_7
+	Bb7
+	B_7
+.ENDENUM
+
+;channel flags.
+	no_channel_flags_set     = 0
+	channel_octave_jump_set  = 1 << 3
+	channel_triplet_note_set = 1 << 5
+	channel_connect_note_set = 1 << 6
---
+++ macros/audio.asm
@@ -0,0 +1,214 @@
+.MACRO music_header
+	.BYTE $00
+.ENDMACRO
+
+.MACRO triplet_note_set
+	.BYTE $00
+.ENDMACRO
+
+.MACRO connect_note_set
+	.BYTE $01
+.ENDMACRO
+
+.MACRO dotted_note_set
+	.BYTE $02
+.ENDMACRO
+
+.MACRO octave_jump
+	.IF current_octave_jump
+		current_octave_jump .SET 0
+	.ELSE
+		current_octave_jump .SET 1
+	.ENDIF
+	.BYTE $03
+.ENDMACRO
+
+.MACRO channel_flags b
+	.IF b && channel_octave_jump_set
+		current_octave_jump .SET 1
+	.ELSE
+		current_octave_jump .SET 0
+	.ENDIF
+	.BYTE $04, b
+.ENDMACRO
+
+.MACRO tempo b, c
+	.BYTE $05, b, c
+.ENDMACRO
+
+.MACRO note_ads b
+	.BYTE $06, b
+.ENDMACRO
+
+.MACRO volume b
+	.IF b & $0F
+		.BYTE $07, b
+	.ELSE
+		.ERROR "Invaild volume."
+	.ENDIF
+.ENDMACRO
+
+.MACRO instrument b
+	.BYTE $08, b
+.ENDMACRO
+
+.MACRO octave b
+	current_octave .SET b
+	.BYTE $09, b
+.ENDMACRO
+
+.MACRO global_transpose b
+	current_global_transpose .SET b
+	.BYTE $0A, b
+.ENDMACRO
+
+.MACRO channel_transpose b
+	current_channel_transpose .SET b
+	.BYTE $0B, b
+.ENDMACRO
+
+.MACRO pitch_tune b
+	.BYTE $0C, b
+.ENDMACRO
+
+.MACRO pitch_slide b
+	.BYTE $0D, b
+.ENDMACRO
+
+.MACRO loop_1 b, c
+	.IF b
+		.BYTE $0E, b
+		.DBYT c
+	.ELSE
+		.ERROR "loop_1 cannot be 0, Please use music_jump."
+	.ENDIF
+.ENDMACRO
+
+.MACRO loop_2 b, c
+	.IF b
+		.BYTE $0F, b
+		.DBYT c
+	.ELSE
+		.ERROR "loop_2 cannot be 0, Please use music_jump."
+	.ENDIF
+.ENDMACRO
+
+.MACRO loop_3 b, c
+	.IF b
+		.BYTE $10, b
+		.DBYT c
+	.ELSE
+		.ERROR "loop_3 cannot be 0, Please use music_jump."
+	.ENDIF
+.ENDMACRO
+
+.MACRO loop_4 b, c
+	.IF b
+		.BYTE $11, b
+		.DBYT c
+	.ELSE
+		.ERROR "loop_4 cannot be 0, Please use music_jump."
+	.ENDIF
+.ENDMACRO
+
+.MACRO break_1 b, c
+	.BYTE $12, b
+	.DBYT c
+.ENDMACRO
+
+.MACRO break_2 b, c
+	.BYTE $13, b
+	.DBYT c
+.ENDMACRO
+
+.MACRO break_3 b, c
+	.BYTE $14, b
+	.DBYT c
+.ENDMACRO
+
+.MACRO break_4 b, c
+	.BYTE $15, b
+	.DBYT c
+.ENDMACRO
+
+.MACRO music_jump b
+	.BYTE $16
+	.DBYT b
+.ENDMACRO
+
+.MACRO music_end
+	.BYTE $17
+.ENDMACRO
+
+.MACRO duty_cycle b
+	.BYTE $18, b << 6
+.ENDMACRO
+
+.MACRO note b, c
+	@d .SET b - current_octave_jump * $18 - current_octave * $0C - current_global_transpose - current_channel_transpose + 1
+		.IF @d > 0 && @d < $20
+			.IF c = 1
+				.BYTE @d | $20
+			.ELSEIF c = 2
+				.BYTE @d | $40
+			.ELSEIF c = 4
+				.BYTE @d | $60
+			.ELSEIF c = 8
+				.BYTE @d | $80
+			.ELSEIF c = 16
+				.BYTE @d | $A0
+			.ELSEIF c = 32
+				.BYTE @d | $C0
+			.ELSEIF c = 64
+				.BYTE @d | $E0
+			.ELSE
+				.ERROR "Invaild note length."
+			.ENDIF
+		.ELSE
+			.ERROR "Note is out of bound."
+		.ENDIF
+.ENDMACRO
+
+.MACRO rest b
+	.IF b = 1
+		.BYTE $20
+	.ELSEIF b = 2
+		.BYTE $40
+	.ELSEIF b = 4
+		.BYTE $60
+	.ELSEIF b = 8
+		.BYTE $80
+	.ELSEIF b = 16
+		.BYTE $A0
+	.ELSEIF b = 32
+		.BYTE $C0
+	.ELSEIF b = 64
+		.BYTE $E0
+	.ELSE
+		.ERROR "Invaild note length."
+	.ENDIF
+.ENDMACRO
+
+.MACRO noise_note b, c
+	.IF b > 0 && b < $10
+		.IF c = 1
+			.BYTE b + 1 | $20
+		.ELSEIF c = 2
+			.BYTE b + 1 | $40
+		.ELSEIF c = 4
+			.BYTE b + 1 | $60
+		.ELSEIF c = 8
+			.BYTE b + 1 | $80
+		.ELSEIF c = 16
+			.BYTE b + 1 | $A0
+		.ELSEIF c = 32
+			.BYTE b + 1 | $C0
+		.ELSEIF c = 64
+			.BYTE b + 1 | $E0
+		.ELSE
+			.ERROR "Invaild note length."
+		.ENDIF
+	.ELSE
+		.ERROR "Note is out of bound."
+	.ENDIF
+.ENDMACRO
