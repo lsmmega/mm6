94e53eb01c6cff4fa4b7b36ba75b4dfa2f8c6550
lsmmega <pogmegalsm@outlook.com>
2026-01-01 21:21:42 +0800

    sfx 3f done

--- audio.asm
+++ audio.asm
@@ -67,5 +67,6 @@
 .INCLUDE "audio/sfx/jump_into_water.asm"
 .INCLUDE "audio/sfx/sfx_3d.asm"
 .INCLUDE "audio/sfx/power_adapter_destroy_ice.asm"
+.INCLUDE "audio/sfx/sfx_3f.asm"
 .INCBIN  "audio/audio_1.bin"
 .INCLUDE "audio/unused/unused.asm"
---
+++ audio/sfx/sfx_3f.asm
@@ -0,0 +1,9 @@
+sfx_3f:
+	sfx_priority 6
+
+;1
+	current_sfx_global_transpose .SET 0
+	sfx_global_channel_flags no_channel_flags_set
+	sfx_frames 0
+	.BYTE 0
+	sfx_end
